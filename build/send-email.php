18a305c6f3221afe7c954305c7b76e5fcaba55fc
Felmomos <balyitortiz0408@gmail.com>
2025-05-21 13:53:30 -0500

    bug fix for email sender

--- build/send-email.php
+++ build/send-email.php
@@ -25,11 +25,20 @@ $mail->Port = 587;
 $mail->Username = "contact@themanagementinc.com";
 $mail->Password = "C0ntact@Beronica2!30";
 
-$mail->setFrom($email, $name);
-$mail->addAddress("balyitortiz0408@gmail.com", "name");
+$mail->setFrom("contact@themanagementinc.com", $name);
+$mail->addAddress("test@themanagementinc.com", "Info");
 
 $mail->Subject = "Message from " . $name;
-$mail->Body = $message;
+$mail->isHTML(true);
+$mail->Body = "<p><strong>Name:</strong> " . $name . "</p>
+    <p><strong>Email:</strong> " . $email . "</p>
+    <p><strong>Phone:</strong> " . $phone . "</p>
+    <p><strong>Details:</strong> " . $details . "</p>
+    <p><strong>Message:</strong><br>" . nl2br($message) . "</p>
+";$message;
 
 $mail->send();
 echo "email sent";
+header("Location: " . $_SERVER["HTTP_REFERER"]);
+exit();
+?>
